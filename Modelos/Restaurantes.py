f5e54becf061b3e9e9a7bacfcc3cf58b563e43d9
TFabri970sw <154623272+TFabri970sw@users.noreply.github.com>
2024-07-09 13:57:28 -0300

    Class

--- Modelos/Restaurantes.py
+++ Modelos/Restaurantes.py
@@ -1,16 +1,21 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+    
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def lista_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
 
-restaurante_noma = Restaurante()
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiano')
 
-restaurantes = [restaurante_praca, restaurante_noma]
-
-#print(dir(restaurante_praca))
-print(vars(restaurante_praca))
+Restaurante.lista_restaurantes()
