2523d4fdf98eca5227d1d613c24fddcc9a12a6ed
TFabri970sw <154623272+TFabri970sw@users.noreply.github.com>
2024-07-10 10:51:57 -0300

    Self

---
+++ Curso-Python-2/Atividades.py
@@ -0,0 +1,10 @@
+class Musica:
+    def __init__(self, nome='', artista='', duracao=0):
+        self.nome = nome
+        self.artista = artista
+        self.duracao = duracao
+
+musica_1 = Musica(nome='Sweet Child OMine', artista='Guns NRoses', duracao=356)
+musica_2 = Musica(nome='Black in Black', artista='AC/DC', duracao=255)
+musica_3 = Musica(nome='I Was Mode For Lovin You', artista='KISS', duracao=271)
+
